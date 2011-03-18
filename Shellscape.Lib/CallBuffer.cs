0955617882243e123323c9345ebf36765c553b62
Andrew Powell <andrew@shellscape.org>
2011-03-18 18:25:08 -0400

    adding support for knowing when a user stops typing

---
+++ Shellscape.Lib/CallBuffer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Shellscape {
+
+	/// <summary>
+	/// Used for buffering text input. eg. we want to be notified when the user stops inputting text, rather than being notified on every single change.
+	/// </summary>
+	public sealed class CallBuffer {
+
+		private TimeSpan _timeSpan;
+		private Timer _timer;
+		private Action _call = null;
+
+		public CallBuffer(Action call, int milliseconds) {
+			_timeSpan = TimeSpan.FromMilliseconds(milliseconds);
+			_call = call;
+		}
+
+		public void Buffer() {
+
+			this.Active = true;
+			
+			if (_timer == null) {
+				_timer = new Timer( new TimerCallback(Finished));
+				_timer.Change(_timeSpan, TimeSpan.Zero);
+			}
+			else {
+				_timer.Change(_timeSpan, TimeSpan.FromMilliseconds(-1));
+			}
+		}
+
+		private void Finished(object state){
+			_timer.Dispose();
+			_timer = null;
+
+			this.Active = false;
+
+			_call.Invoke();
+		}
+
+		public Boolean Active { get; private set; }
+	}
+}
---
+++ Shellscape.Lib/UI/Controls/InputBufferedTextBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Shellscape.UI.Controls {
+
+	/// <summary>
+	/// This TextBox will only notify of TextChanged when it's perceived that the user has stopped entering data.
+	/// </summary>
+	public class InputBufferedTextBox : TextBox {
+
+		private CallBuffer _buffer;
+
+		public InputBufferedTextBox() : base() {
+			_buffer = new CallBuffer(NotifyChanged, 1000);
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e) {
+			_buffer.Buffer();
+			base.OnKeyDown(e);
+		}
+
+		protected override void OnTextChanged(EventArgs e) {
+			if(_buffer.Active){
+				return;
+			}
+			base.OnTextChanged(e);
+		}
+
+		private void NotifyChanged() {
+			if (this.InvokeRequired) {
+				this.Invoke(new Action(delegate() {
+					base.OnTextChanged(EventArgs.Empty);
+				}));
+				return;
+			}
+			base.OnTextChanged(EventArgs.Empty);
+		}
+
+	}
+}
