742e7fdeef3c27fdd11aa0038fc81428fff85654
Andrew Powell <andrew@shellscape.org>
2011-05-15 15:56:04 -0400

    not really used, but holding up the build process

---
+++ Shellscape.Lib/AssemblyResourceNamespaceAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shellscape {
+
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class AssemblyResourceNamespaceAttribute : Attribute {
+
+		public AssemblyResourceNamespaceAttribute(String @namespace) {
+			this.Namespace = @namespace;
+		}
+
+		public String Namespace { get; private set; }
+
+	}
+}
