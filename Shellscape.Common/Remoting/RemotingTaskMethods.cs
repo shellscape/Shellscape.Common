c1f82e3d4b7c5b6f35c9eacf5d1632de911492eb
shellscape <andrew@shellscape.org>
2011-10-06 19:27:31 -0400

    more remoting / jumplist reorg

---
+++ Shellscape.Common/Remoting/RemotingTaskMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shellscape.Remoting {
+	public static class RemotingTaskMethods {
+
+		static RemotingTaskMethods() {
+			RemotingTaskMethods.Methods = new Dictionary<String, RemoteTaskMethod>();
+		}
+
+		public static Dictionary<String, RemoteTaskMethod> Methods { get; private set; }
+
+	}
+}
