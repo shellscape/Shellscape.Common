8b5c3f9c920cc0ce2db2f95d9ed8343fb7a4b24a
shellscape <andrew@shellscape.org>
2012-06-11 10:42:17 -0400

    fixing namespace

--- Shellscape.Common/Extensions.cs
+++ Shellscape.Common/Extensions.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.Windows.Forms.ComponentModel;
 
-namespace GmailNotifierPlus {
+namespace Shellscape {
 	public static class Extensions {
 
 		public static IEnumerable<Control> All(this Control.ControlCollection controls) {
