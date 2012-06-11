d4b6784d124d09f4f35297d1431ef27daa690659
shellscape <andrew@shellscape.org>
2012-06-11 11:02:02 -0400

    repairing the damage done by smartgit, restoring update manager

--- Shellscape.Common/UpdateManager.cs
+++ Shellscape.Common/UpdateManager.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Net;
+using System.Reflection;
 using System.Threading;
 using System.Web.Script.Serialization;
+
+using Ionic.Zip;
 
 namespace Shellscape {
 
@@ -47,31 +51,34 @@ namespace Shellscape {
 			public int id { get; set; }
 		}
 
-		private readonly int _updateInterval = 300000; // 5 minutes
-		private const String _api = "https://api.github.com/repos/{0}/{1}/downloads";
-
-		//private WebRequest _webRequest;
+		private  String _api = "https://api.github.com/repos/{0}/{1}/downloads";
 		private Timer _timer;
 		private static UpdateManager _current = null;
 
-		private UpdateManager() {
-			System.Reflection.Assembly assembly =  System.Reflection.Assembly.GetEntryAssembly();
-			this.CurrentVersion = assembly != null ? assembly.GetName().Version.ToString() : "1.0.0.0";
+		public delegate void UpdateManagerEventHandler(UpdateManager sender);
 
+		private UpdateManager() {
+			this._api = "https://api.github.com/repos/{0}/{1}/downloads";
+			Assembly entryAssembly = Assembly.GetEntryAssembly();
+			this.CurrentVersion = (entryAssembly != null) ? entryAssembly.GetName().Version.ToString() : "1.0.0.0";
 			_current = this;
+			this.UpdateInterval = 3600000; // 1 hour
 		}
 
-		public UpdateManager(String user, String repository, String appName) : this() {
+		public UpdateManager(string user, string repository, string appName) : this() {
 			this.User = user;
 			this.Repository = repository;
-			this.AppName = String.Concat(appName, "-"); // this is the file name format i use; [name]-[version].zip
+			this.AppName = appName + "-";
 		}
 
 		public event UnhandledExceptionEventHandler Error;
+		public event UpdateManagerEventHandler UpdateAvailable;
+
+		public string AppName { get; private set; }
 
 		public static UpdateManager Current {
 			get {
-				if (_current == null) {
+				if(_current == null) {
 					new UpdateManager();
 				}
 
@@ -79,156 +86,163 @@ namespace Shellscape {
 			}
 		}
 
-		public UpdateStatus Status { get; private set; }
-		public String AppName { get; private set; }
-		public String CurrentVersion { get; private set; }
-		public String User { get; private set; }
-		public String Repository { get; private set; }
-
+		public string CurrentVersion { get; private set; }
 		public LatestVersion Latest { get; private set; }
-
-		public void StartTimer() {
-			_timer = new Timer(delegate(object state){
-				
-				this.Start();
-				
-			}, this, 0, _updateInterval);
+		public string NewVersion { get; private set; }
+		public string Repository { get; private set; }
+		public UpdateStatus Status { get; private set; }
+		public int UpdateInterval { get; set; }
+		public string User { get; private set; }
+		
+		private void Check(string data) {
+			if(string.IsNullOrEmpty(data)) {
+				this.Status = UpdateStatus.Problem;
+			}
+			else {
+				List<GitHubDownload> list = new List<GitHubDownload>();
+				JavaScriptSerializer serializer = new JavaScriptSerializer();
+				try {
+					list = serializer.Deserialize<List<GitHubDownload>>(data);
+				}
+				catch(Exception exception) {
+					this.Status = UpdateStatus.Problem;
+					if(this.Error != null) {
+						this.Error(this, new UnhandledExceptionEventArgs(exception, false));
+					}
+				}
+				if(list.Count == 0) {
+					this.Status = UpdateStatus.Problem;
+				}
+				else {
+					GitHubDownload download = (from d in list
+																		 where !d.name.Contains("debug")
+																		 select d).First<GitHubDownload>();
+					FileInfo info = new FileInfo(download.name);
+					string remoteVersion = info.Name.Replace(this.AppName, string.Empty).Replace(info.Extension, string.Empty);
+					Version current = new Version(this.CurrentVersion);
+					Version remote = new Version(remoteVersion);
+					
+					this.Latest = new LatestVersion(download.html_url, download.name, remoteVersion);
+					
+					if(current < remote) {
+						this.Status = UpdateStatus.NewVersion;
+						this.NewVersion = remote.ToString();
+						if(this.UpdateAvailable != null) {
+							this.UpdateAvailable(this);
+						}
+					}
+					else {
+						this.Status = UpdateStatus.UpToDate;
+					}
+				}
+			}
 		}
 
-		public void Stop() {
-			_timer.Dispose();
-			this.CancelAndWait();
-		}
-
-		public new void Start() {
-			if (this.IsDone) {
+		public void CheckOnce() {
+			if(base.IsDone) {
 				base.Start();
 			}
 		}
 
-		public void Download(String tempPath, System.ComponentModel.AsyncCompletedEventHandler callback) {
+		public void Download(string dataPath, AsyncCompletedEventHandler callback) {
 			try {
-				using (WebClient webClient = new WebClient()) {
-					webClient.DownloadFileCompleted += callback;
-					webClient.DownloadFileAsync(new Uri(String.Format(_api, this.User, this.Repository)), Path.Combine(tempPath, "Updates", this.Latest.FileName));
+				using(WebClient client = new WebClient()) {
+					FileInfo dest = new FileInfo(Path.Combine(dataPath, "Updates", this.Latest.FileName));
+					if(!dest.Directory.Exists) {
+						dest.Directory.Create();
+					}
+					client.DownloadFileCompleted += delegate(object sender, AsyncCompletedEventArgs e) {
+						if(!e.Cancelled && (e.Error == null)) {
+							this.Unzip(dest.FullName);
+							callback(sender, e);
+						}
+					};
+					client.DownloadFileAsync(new Uri(this.Latest.FileUrl), dest.FullName);
 				}
 			}
-			catch (WebException e) {
-				if (Error != null) {
-					Error.Invoke(this, new UnhandledExceptionEventArgs(e, false));
+			catch(WebException exception) {
+				if(this.Error != null) {
+					this.Error(this, new UnhandledExceptionEventArgs(exception, false));
 				}
 				this.Status = UpdateStatus.Problem;
 			}
 		}
 
 		protected override void DoWork() {
+			this.Request();
+			if(base.CancelRequested) {
+				base.AcknowledgeCancel();
+			}
+		}
 
-			//while (!CancelRequested) {
-			Request();
-				//System.Threading.Thread.Sleep(this._updateInterval);
-			//}
+		public void Replace(string dataPath, string appPath) {
+			DirectoryInfo source = new DirectoryInfo(Path.Combine(dataPath, "Updates", this.NewVersion));
+			this.ReplaceDirectory(source, new DirectoryInfo(appPath));
+		}
+
+		private void ReplaceDirectory(DirectoryInfo source, DirectoryInfo dest) {
 			
-			if (CancelRequested) {
-				AcknowledgeCancel();
+			if(!dest.Exists) {
+				dest.Create();
+			}
+			
+			FileInfo[] files = source.GetFiles();
+			
+			foreach(FileInfo file in files) {
+				string path = Path.Combine(dest.FullName, file.Name);
+				if(File.Exists(path)) {
+					file.Replace(path, path + "." + this.CurrentVersion, true);
+				}
+				else {
+					file.CopyTo(path);
+				}
+			}
+			
+			DirectoryInfo[] directories = source.GetDirectories();
+			
+			foreach(DirectoryInfo dir in directories) {
+				this.ReplaceDirectory(dir, new DirectoryInfo(Path.Combine(dest.FullName, dir.Name)));
 			}
 		}
 
 		private void Request() {
-
 			this.Status = UpdateStatus.Checking;
-
-			String data = String.Empty;
-
+			string data = string.Empty;
 			try {
-				using (WebClient webClient = new WebClient()) {
-					data = webClient.DownloadString(String.Format(_api, this.User, this.Repository));
+				using(WebClient client = new WebClient()) {
+					data = client.DownloadString(string.Format(this._api, this.User, this.Repository));
 				}
 			}
-			catch (WebException e) {
-				if (Error != null) {
-					Error.Invoke(this, new UnhandledExceptionEventArgs(e, false));
+			catch(WebException exception) {
+				if(this.Error != null) {
+					this.Error(this, new UnhandledExceptionEventArgs(exception, false));
 				}
 				this.Status = UpdateStatus.Problem;
 			}
-
-			System.Threading.Thread.Sleep(5000);
-
-			Check(data);
-
-			//try {
-			//  HttpWebResponse response = _webRequest.GetResponse() as HttpWebResponse;
-
-			//  if (response.StatusCode == HttpStatusCode.OK) {
-
-			//    String data = String.Empty;
-
-			//    using (Stream stream = response.GetResponseStream()) {
-
-			//      if (stream == null || !stream.CanRead) {
-			//        this.Status = UpdateStatus.Problem;
-			//        Debug.WriteLine("Stream Problem");
-			//        return;
-			//      }
-
-			//      using (StreamReader reader = new StreamReader(stream)) {
-			//        data = reader.ReadToEnd();
-			//      }
-			//    }
-
-			//    Check(data);
-			//  }
-			//}
-			//catch (Exception e) {
-			//  if (Error != null) {
-			//    Error.Invoke(this, new UnhandledExceptionEventArgs(e, false));
-			//  }
-			//  this.Status = UpdateStatus.Problem;
-			//}
-
+			Thread.Sleep(0x1388);
+			this.Check(data);
 		}
 
-		private void Check(String data) {
+		public new void Start() {
+			this._timer = new Timer(delegate(object state) {
+				this.CheckOnce();
+			}, this, 0x2710, this.UpdateInterval);
+		}
 
-			if (String.IsNullOrEmpty(data)) {
-				this.Status = UpdateStatus.Problem;
-				return;
-			}
-		
-			List<GitHubDownload> downloads = new List<GitHubDownload>();
-			JavaScriptSerializer serializer = new JavaScriptSerializer();
+		public void Stop() {
+			this._timer.Dispose();
+			base.Cancel();
+		}
 
-			try {
-				downloads = serializer.Deserialize<List<GitHubDownload>>(data);
-			}
-			catch (Exception e) {
-				
-				this.Status = UpdateStatus.Problem;
-
-				if (Error != null) {
-					Error.Invoke(this, new UnhandledExceptionEventArgs(e, false));
+		private void Unzip(string zipFile) {
+			FileInfo zip = new FileInfo(zipFile);
+			DirectoryInfo dir = zip.Directory.CreateSubdirectory(this.NewVersion);
+			using(ZipFile file = ZipFile.Read(zip.FullName)) {
+				foreach(ZipEntry entry in file) {
+					entry.Extract(dir.FullName, ExtractExistingFileAction.OverwriteSilently);
 				}
 			}
-
-			if (downloads.Count == 0) {
-				this.Status = UpdateStatus.Problem;
-				return;
-			}
-
-			GitHubDownload latest = downloads.Where(d => !d.name.Contains("debug")).First();
-			FileInfo file = new FileInfo(latest.name);
-			String version = file.Name.Replace(this.AppName, String.Empty).Replace(file.Extension, String.Empty);
-
-			Version current = new Version(this.CurrentVersion);
-			Version remote = new Version(version);
-
-			this.Latest = new LatestVersion(latest.html_url, latest.name, version);
-
-			if(current < remote){
-				this.Status = UpdateStatus.NewVersion;
-			}
-			else{
-				this.Status = UpdateStatus.UpToDate;
-			}
+			zip.Delete();
 		}
 	}
 }
